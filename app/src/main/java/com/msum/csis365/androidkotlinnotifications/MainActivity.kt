a6e8ce179744c42e6f554103ace58a0ce56a8c28
Kyle <kdahn@bushelpowered.com>
2020-05-19 14:58:44 -0500

    Added Codelab steps

--- app/src/main/java/com/msum/csis365/androidkotlinnotifications/MainActivity.kt
+++ app/src/main/java/com/msum/csis365/androidkotlinnotifications/MainActivity.kt
@@ -8,5 +8,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createNotificationChannel()
+
+        // TODO - Step 5 - Send the Notification when the Floating Action Button is clicked
+
+    }
+
+    private fun createNotificationChannel() {
+        // TODO - Step 2 - Create a Notification Channel
+
+        // TODO - Step 3 - Retrieve the Notification Manager System Service
+
+    }
+
+    private fun buildNotification() {
+        // TODO - Step 4 - Build a Notification
+
     }
 }
